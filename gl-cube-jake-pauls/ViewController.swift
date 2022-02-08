429e9c33af4817ac17be71eb815143b0956184d7
jake-pauls <jakepauls99@gmail.com>
2022-02-08 10:17:29 -0800

    Fixed file pathing issues, added appropriate ViewController file

--- gl-cube-jake-pauls/ViewController.swift
+++ gl-cube-jake-pauls/ViewController.swift
@@ -1,19 +1,72 @@
 //
-//  ViewController.swift
-//  gl-cube-jake-pauls
-//
-//  Created by Jake Pauls on 2022-02-07.
+// ViewController.swift
+// Created by Jake Pauls
 //
 
-import UIKit
+import GLKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+extension ViewController: GLKViewControllerDelegate {
+    func glkViewControllerUpdate(_ viewController: GLKViewController) {
+        viewRenderer.update()
+        self.updateUILabels()
     }
-
-
 }
 
+class ViewController: GLKViewController {
+    private var context: EAGLContext?
+    private var viewRenderer: ViewRenderer!
+    private var referencePoint: CGPoint!
+    private var gestureSet: GestureSet!
+    
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var rotationLabel: UILabel!
+    
+    @IBAction func onResetPress(_ sender: UIButton) {
+        viewRenderer.transform.reset()
+    }
+    
+    func updateUILabels() {
+        let rotX = String(format: "%.2f", viewRenderer.transform.rotX)
+        let rotY = String(format: "%.2f", viewRenderer.transform.rotY)
+        
+        let rotString = "Rot: (\(rotX), \(rotY), 0.0)"
+        rotationLabel.text = rotString
+        
+        let posX = String(format: "%.2f", viewRenderer.transform.posX)
+        let posY = String(format: "%.2f", viewRenderer.transform.posY)
+        
+        let posString = "Pos: (\(posX), \(posY), 0.0)"
+        positionLabel.text = posString
+    }
+    
+    private func setupGLView() {
+        context = EAGLContext(api: .openGLES3)
+        EAGLContext.setCurrent(context)
+        
+        if let view = self.view as? GLKView, let context = context {
+            view.context = context
+            delegate = (self as GLKViewControllerDelegate)
+                        
+            // Setup and load view renderer
+            viewRenderer = ViewRenderer()
+            viewRenderer.setup(view)
+            viewRenderer.load()
+            
+            // Initialize gesture set
+            gestureSet = GestureSet(viewRenderer: viewRenderer)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupGLView()
+        
+        // Setup gesture recognizers for the UIView
+        gestureSet.setupGestureRecognizers(view: view)
+        
+    }
+    
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        viewRenderer.draw(rect)
+    }
+}
